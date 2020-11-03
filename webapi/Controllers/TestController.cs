4edb00022d172ff5b5a358d7d25ced955320ff68
Ramaprasad Udumalpet Palanichamy <ram.up3@gmail.com>
2020-11-02 20:53:10 -0600

    Add project files.

---
+++ webapi/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webapi.Controllers
+{
+    public class TestController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome To Web API";
+        }
+        public List<string> Get(int Id)
+        {
+            return new List<string> {
+                "Data1",
+                "Data2"
+            };
+        }
+    }
+}
